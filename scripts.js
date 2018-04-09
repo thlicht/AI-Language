2470081cb1f4557ca4b52cb6aab97aa6a3a2391e
thlicht <tlicht93@live.com>
2018-04-09 15:18:09 -0400

    probability framework in place

--- scripts.js
+++ scripts.js
@@ -2,6 +2,10 @@ var GermanWords = ["das", "ist", "du", "ich", "nicht", "die", "es", "und", "Sie"
 var SpanWords = ["vez", "año", "tiempo", "dia", "cosa", "ser", "haber", "estar", "tener", "hacer", "su", "lo", "todo", "más", "este", "ya", "muy", "también", "así", "sí", "que", "y", "como", "pero", "o"];
 var EngWords = ["the", "be", "to", "of", "and", "a", "in", "that", "have", "I", "it", "for", "not", "on", "with", "he", "as", "you", "do", "at", "this", "but", "his", "by", "from"];
 var FrenWords = ["Le", "De", "Un", "À", "Être", "Et", "En", "Avoir", "Que", "Pour", "Dans", "Ce", "Il", "Qui", "Ne", "Sur", "Se", "Pas", "Plus", "Pouvoir", "Par", "Je", "Avec", "Tout", "Faire"];
+var GerFreq = { "a": 1, "b": 2, "c": 3, "d": 4, "e": 5, "f": 6, "g": 7, "h": 8, "i": 9, "j": 10, "k": 11, "l": 12, "m": 13, "n": 14, "o": 15, "p": 16, "q": 17, "r": 18, "s": 19, "t": 20, "u": 21, "v": 22, "w": 23, "x": 24, "y": 25, "z": 26 };
+var EngFreq = { "a": 1, "b": 2, "c": 3, "d": 4, "e": 5, "f": 6, "g": 7, "h": 8, "i": 9, "j": 10, "k": 11, "l": 12, "m": 13, "n": 14, "o": 15, "p": 16, "q": 17, "r": 18, "s": 19, "t": 20, "u": 21, "v": 22, "w": 23, "x": 24, "y": 25, "z": 26 };
+var FrenFreq = { "a": 1, "b": 2, "c": 3, "d": 4, "e": 5, "f": 6, "g": 7, "h": 8, "i": 9, "j": 10, "k": 11, "l": 12, "m": 13, "n": 14, "o": 15, "p": 16, "q": 17, "r": 18, "s": 19, "t": 20, "u": 21, "v": 22, "w": 23, "x": 24, "y": 25, "z": 26 };
+var SpanFreq = { "a": 1, "b": 2, "c": 3, "d": 4, "e": 5, "f": 6, "g": 7, "h": 8, "i": 9, "j": 10, "k": 11, "l": 12, "m": 13, "n": 14, "o": 15, "p": 16, "q": 17, "r": 18, "s": 19, "t": 20, "u": 21, "v": 22, "w": 23, "x": 24, "y": 25, "z": 26 };
 var compareWords = function (wordstoCompare, word) {
     for (var i = 0; i < 25; i++) {
         if (word == wordstoCompare[i]) {
@@ -15,6 +19,18 @@ var InputTracker = /** @class */ (function () {
         this.position = 0;
         this.word = "";
     }
+    InputTracker.prototype.heuristic = function (word) {
+        var GerPercent = 0;
+        var EngPercent = 0;
+        var SpanPercent = 0;
+        var FrenPrecent = 0;
+        word = word.toLocaleLowerCase();
+        for (var i = 0; i < word.length; i++) {
+            if (i == 0) {
+                alert(GerFreq["c"]);
+            }
+        }
+    };
     //attempt to make a determination of for the language of the paragraph
     InputTracker.prototype.process = function (text, key) {
         this.word = text.substring(this.position, text.length - 1);
@@ -25,6 +41,7 @@ var InputTracker = /** @class */ (function () {
             var f = compareWords(FrenWords, this.word);
             var e = compareWords(EngWords, this.word);
             var s = compareWords(SpanWords, this.word);
+            this.heuristic(text);
             if (g) {
                 alert("German");
                 return;
--- scripts.ts
+++ scripts.ts
@@ -3,6 +3,16 @@ const SpanWords: Array<string> = ["vez", "año", "tiempo", "dia", "cosa" ,"ser",
 const EngWords: Array<string> = ["the","be","to","of","and","a","in","that","have","I","it","for","not","on","with","he","as","you","do","at","this","but","his","by","from"];
 const FrenWords: Array<string> = ["Le","De","Un","À","Être","Et","En","Avoir","Que","Pour","Dans","Ce","Il","Qui","Ne","Sur","Se","Pas","Plus","Pouvoir","Par","Je","Avec","Tout","Faire"];
 
+interface LetterToFreq {
+    [letter:string] : number;
+}
+
+const GerFreq:LetterToFreq = { "a" : 1, "b" : 2, "c": 3, "d" : 4, "e":5, "f":6, "g":7, "h":8, "i":9 ,"j":10, "k":11, "l":12, "m":13,"n":14,"o":15,"p":16, "q":17, "r":18, "s":19, "t":20, "u":21, "v":22, "w":23, "x":24, "y":25, "z":26};
+const EngFreq:LetterToFreq = { "a" : 1, "b" : 2, "c": 3, "d" : 4, "e":5, "f":6, "g":7, "h":8, "i":9 ,"j":10, "k":11, "l":12, "m":13,"n":14,"o":15,"p":16, "q":17, "r":18, "s":19, "t":20, "u":21, "v":22, "w":23, "x":24, "y":25, "z":26};
+const FrenFreq:LetterToFreq = { "a" : 1, "b" : 2, "c": 3, "d" : 4, "e":5, "f":6, "g":7, "h":8, "i":9 ,"j":10, "k":11, "l":12, "m":13,"n":14,"o":15,"p":16, "q":17, "r":18, "s":19, "t":20, "u":21, "v":22, "w":23, "x":24, "y":25, "z":26};
+const SpanFreq:LetterToFreq = { "a" : 1, "b" : 2, "c": 3, "d" : 4, "e":5, "f":6, "g":7, "h":8, "i":9 ,"j":10, "k":11, "l":12, "m":13,"n":14,"o":15,"p":16, "q":17, "r":18, "s":19, "t":20, "u":21, "v":22, "w":23, "x":24, "y":25, "z":26};
+
+
 let compareWords = (wordstoCompare: Array<string>, word: string):boolean => {
     
     for (let i = 0;i < 25; i++)
@@ -24,6 +34,22 @@ class InputTracker {
         this.position = 0;
         this.word = "";
     }
+
+    heuristic(word: string){
+        let GerPercent = 0;
+        let EngPercent = 0;
+        let SpanPercent = 0;
+        let FrenPrecent = 0;
+        word = word.toLocaleLowerCase();
+        for (let i = 0; i < word.length; i++)
+        {
+            if (i == 0)
+            {
+                alert(GerFreq["c"]);
+            }    
+        }
+    }
+
     //attempt to make a determination of for the language of the paragraph
     process(text: string, key: number):void {
         this.word = text.substring(this.position,text.length-1);
@@ -35,6 +61,8 @@ class InputTracker {
             let f = compareWords(FrenWords, this.word);
             let e = compareWords(EngWords, this.word);
             let s = compareWords(SpanWords, this.word);
+            this.heuristic(text);
+
 
             if(g)
             {
@@ -61,6 +89,8 @@ class InputTracker {
             return;
         }
     }
+
+    
 }
 
 let tracker = null;
